a9dcbc5784db0a4b8a0af8f688df1819cc4e9e5b
AlexTrinityBlock <q123717111@gmail.com>
2022-10-26 11:23:50 +0800

    引入ECDSA 模塊

--- codes/bank-django-service/app_core/tests/test_algorithm.py
+++ codes/bank-django-service/app_core/tests/test_algorithm.py
@@ -1,9 +1,15 @@
 from django.test import TestCase
+import os
+import json
+from ellipticcurve.ecdsa import Ecdsa
+from ellipticcurve.privateKey import PrivateKey, PublicKey
 from ..models import YiModifiedPaillierEncryptionPy
 
 class TestAlgorithm(TestCase):
     
     def setUp(self):
+        self.ECDSA_PUBLICKEY = os.environ['ECDSA_PUBLICKEY']
+        self.ECDSA_PRIVATEKEY = os.environ['ECDSA_PRIVATEKEY']
         pass
 
     # 測試Yi算法
@@ -11,3 +17,10 @@ class TestAlgorithm(TestCase):
         print("[算法測試] 測試Yi同態加密")
         yiModifiedPaillierEncryptionPy = YiModifiedPaillierEncryptionPy()
         yiModifiedPaillierEncryptionPy.test()
+
+    def test_ECDSA(self):
+        privateKey = PrivateKey.fromPem(self.ECDSA_PRIVATEKEY)
+        publicKey = PublicKey.fromPem(self.ECDSA_PUBLICKEY)
+        message = json.dumps({"data": "123"})
+        signature = Ecdsa.sign(message, privateKey)
+        print(Ecdsa.verify(message, signature, publicKey))
