b7edc7228275d869db396e16aef9a74568c75a0b
AlexTrinityBlock <q123717111@gmail.com>
2022-10-26 11:27:10 +0800

    ECDSA 模塊測試

--- codes/bank-django-service/app_core/tests/test_algorithm.py
+++ codes/bank-django-service/app_core/tests/test_algorithm.py
@@ -18,9 +18,12 @@ class TestAlgorithm(TestCase):
         yiModifiedPaillierEncryptionPy = YiModifiedPaillierEncryptionPy()
         yiModifiedPaillierEncryptionPy.test()
 
+    # 測試ECDSA模塊
     def test_ECDSA(self):
+        print("[算法測試] ECDSA模塊")
         privateKey = PrivateKey.fromPem(self.ECDSA_PRIVATEKEY)
         publicKey = PublicKey.fromPem(self.ECDSA_PUBLICKEY)
         message = json.dumps({"data": "123"})
         signature = Ecdsa.sign(message, privateKey)
-        print(Ecdsa.verify(message, signature, publicKey))
+        result = Ecdsa.verify(message, signature, publicKey)
+        self.assertTrue(result, "\n\n ECDSA模塊測試失敗，有可能是模塊損壞或者ECDSA鑰匙錯誤")
