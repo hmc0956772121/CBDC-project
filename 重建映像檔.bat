cfa1fa020bdfb8b8cf3244ad8661363822596198
AlexTrinityBlock <q123717111@gmail.com>
2022-11-08 21:52:29 +0800

    重建映像檔更新

--- 重建映像檔.bat
+++ 重建映像檔.bat
@@ -1,9 +1,6 @@
 docker-compose down
 
-docker rm -f cbdc-project-bank-django-service-1
-docker rm -f cbdc-project-swagger-server-1
-docker rm -f cbdc-project-bank-database-service-1
-docker rm -f  cbdc-project-bank-redis-service-1
+docker rm $(docker ps -aq --filter name=cbdc*)
 
 docker-compose up -d --build --force-recreate
 docker-compose stop
