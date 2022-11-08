07b99ed3c3249b727a02de882ce1dc3776998c3b
AlexTrinityBlock <q123717111@gmail.com>
2022-11-08 21:42:33 +0800

    更新重建映像檔

--- 重建映像檔.bat
+++ 重建映像檔.bat
@@ -1,3 +1,9 @@
 docker-compose down
+
+docker rm -f cbdc-project-bank-django-service-1
+docker rm -f cbdc-project-swagger-server-1
+docker rm -f cbdc-project-bank-database-service-1
+docker rm -f  cbdc-project-bank-redis-service-1
+
 docker-compose up -d --build --force-recreate
 docker-compose stop
